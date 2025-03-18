8d2f6cda122f2a19ceef1f684b58de12e46c5c94
AliaksandraVasilevich <aleksandravasilevic9@gmail.com>
2025-03-18 12:06:57 +0300

    added styles to main.css, modified markup in Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import styles from '..//styles/Main.module.css'
+// import styles from '..//styles/Main.module.css'
 import {Link} from 'react-router-dom'
-
+import "../styles/main.css";
 
 
 const FIELDS = {
@@ -18,25 +18,35 @@ const Main = () => {
   //console.log(values);
 
   return (
-    <div className={styles.wrap}>
-      <div className={styles.container}>
-        <h1 className={styles.heading}>JOIN</h1>
-        <form className={styles.form}>
-          <div className={styles.group}>
-            <h4>Login</h4>
-            <input type="text" name="username" value={values[USERNAME]} className={styles.input} onChange={handleChange} autoComplete="off" required/>
-            <h4>Password</h4>
-            <input type="text" name="password" value={values[PASSWORD]} className={styles.input} onChange={handleChange} autoComplete="off" required/>
-        
+    <><header className='header'>
+      <h2 className="header-title">Chat name</h2>
+    </header>
+    <main className='content'>
+      <h1 className='visually-hidden'>Chat</h1>
+      <section className='enter'>
+      <h2 className='visually-hidden'>Enter login and password form</h2>
+          <div className='enter-body container'>
+              <form className='enter-form'>
+                <div className='enter-form-body'>
+                  <div className='enter-form-field'>
+                    <label className='enter-form-title' htmlFor="login">Login</label>
+                    <input type="text" id='login' name="username" value={values[USERNAME]} className='enter-form-input input' onChange={handleChange} autoComplete="off" required />
+                  </div>
+                  <div className='enter-form-field'> 
+                    <label className='enter-form-title' htmlFor="password">Password</label>
+                    <input type="text" id='password' name="password" value={values[PASSWORD]} className='enter-form-input input' onChange={handleChange} autoComplete="off" required />
+                  </div>  
+                    <Link className='link' to={`/chat?name=${values[USERNAME]}&password=${values[PASSWORD]}`}>
+                    <button type="submit" className ="enter-form-button">Login
+                    </button>
+                  </Link>
+                </div>  
+              </form>
           </div>
-          <Link to={`/chat?name=${values[USERNAME]}&password=${values[PASSWORD]}`}>
-          <button type="submit" className={styles.button}>Login
-
-          </button>
-        </Link>
-        </form>
-      </div>
-    </div>)
+      </section>
+    </main>
+    <footer></footer>
+   </>)
   
 };
 
