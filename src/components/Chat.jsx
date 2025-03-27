350d00d2b141758c510d7ad2dae71063a813fb4b
Kristina Gordey <kristinababichvic@gmail.com>
2025-03-27 20:03:19 +0300

    added sending messages

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -5,6 +5,7 @@ import EmojiPicker from "emoji-picker-react";
 
 import icon from  "../images/smile.png"
 import styles from "../styles/main.css";
+import Messages  from './Messages';
 
 
 const socket = io.connect("http://localhost:5000");
@@ -35,25 +36,37 @@ const Chat = () => {
   
   const leftRoom =()=>{};
   const handleChange =({target:{value}})=>setMessage(value);
-  const handleSubmit =()=>{};
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    if(!message) return;
+    socket.emit('sendMessage',{message, params});
+    setMessage("");
+  };
   const onEmojiClick =({emoji})=>setMessage(`${message} ${emoji}`);
 
   return (
     <><header className='header'>
         <h2 className="header__title header__title-chat">
           <span>Chat name</span>
-          <span>0 users in this room</span>
+          
+          <div className={styles.users}>
+           0 users in this room
+         </div>
         </h2>
       </header>
     <main className='content'>
       <h1 className='visually-hidden'>Chat</h1>
       <section className='messages container'>
       <h2 className='visually-hidden'>Messages area</h2>
-      <div className="messages__area"></div>
+      <div className="messages__area">
+        <div className={styles.messages}>
+            <Messages messages ={state} name={params.name} />
+            </div>
+        </div>
       <div className="message__enter contain">
-        <form className="message__enter-form">
+        <form className="message__enter-form" onSubmit={handleSubmit}>
             <label htmlFor="message-input" className='visually-hidden'>Message</label>
-            <input type="text" id='message-input' className="message__enter-input input" placeholder='What do you want to say?' />
+            <input type="text" id='message-input' className="message__enter-input input" placeholder='What do you want to say?' value={message} onChange={handleChange} />
             <button title='input' className='message__enter-button' type='submit'>
               <span className="visually-hidden">Send message</span>
             </button>
@@ -64,6 +77,9 @@ const Chat = () => {
     <footer> 
     </footer>
    </>
+
+
+
     // <div className={styles.wrap}>
     //   <div className={styles.header}>
     //     <div className={styles.title}>
@@ -76,10 +92,7 @@ const Chat = () => {
     //       Left the room
     //     </button>
     //   </div>
-    //   <div className={styles.messages}>
-    //     {state.map(({message}, i)=><span key={i}>{message}</span>)}
-
-    //   </div>
+     //  <Messages messages ={state} name={params.name} />
     //   <form className ={styles.form}>
     //     <div className={styles.input}>
     //   <input type="text" name="message" placeholder='What do you want to say?' value={message} onChange={handleChange} autoComplete="off" required />
